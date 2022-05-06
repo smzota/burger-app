ffd2be1dba81d1c0b03fb2596ffc90756c4f0393
samuel <smzota@gmail.com>
2022-05-06 15:12:46 +0200

    add zero choice of ingredient

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,20 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey =>{
             return [...Array(props.ingredients[igKey])].map((_, i)=>{
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             })
         })
+        .reduce((prev, curr)=>{
+            return prev.concat(curr);
+        }, [])
+
+        console.log(transformedIngredients)
+        if(transformedIngredients.length === 0){
+            transformedIngredients = <p>Please add ingredients!</p>
+        }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
